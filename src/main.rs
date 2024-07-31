5fa030ab85d52bfada40baa808f07a688e12fd95
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-07-31 02:47:18 -0400

    The reading has begun

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{stdin, stdout, Write};
+use termion::{clear, cursor};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    write!(stdout, "{}{}Ctrl-C to exit.\n\r",
+           clear::All,
+           cursor::Goto(1, 1))
+        .unwrap();
+    stdout.flush().unwrap();
+
+    for k in stdin().keys() {
+        match k.unwrap() {
+            Key::Ctrl('c') => break,
+            Key::Char('\n') => print!("\n\r"),
+            Key::Char(c) => print!("{}", c),
+            _ => print!("Unknown input")
+        }
+
+        stdout.flush().unwrap();
+    }
+
+    write!(stdout, "{}", clear::All).unwrap();
 }
